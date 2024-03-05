7e4c2ef3e9a678fa54f43d3bc71c3a449e94be7b
anishbelel <134290462+anishbelel@users.noreply.github.com>
2024-03-06 00:43:21 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,7 +1,12 @@
+function closeWindow() {
+    window.close();
+}
 const myButton = document.getElementById("submit");
 
-    myButton.addEventListener("click", function() {
-        alert("Form submitted successfully!"); // Show an alert when the button is clicked
-        alert("Ready to go to the home page?"); // Show an alert when the button is clicked
-        window.location.href = "index.html";
-    });
+myButton.addEventListener("click", function () {
+    alert("Form submitted successfully!"); // Show an alert when the button is clicked
+    alert("Ready to go to the home page?"); // Show an alert when the button is clicked
+    window.location.href = "index.html";
+    closeWindow();
+});
+
